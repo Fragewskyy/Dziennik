6f9d15ad69bd6c81434ba721f54df65842d2ed2b
aloix123 <pawel.kupis@o2.pl>
2021-10-12 16:45:56 +0200

    hej siema dodałem podstawowe modele i ten tego

--- src/main/java/view/MenuView.java
+++ src/main/java/view/MenuView.java
@@ -1,6 +1,22 @@
 package view;
 
+import java.util.Scanner;
+
 public class MenuView {
-
-
+    public void Diaroioptions(){
+        System.out.println("Clik");
+        System.out.println("(x) to logout");
+        System.out.println("(x) to logout");
+    }
+    public static void  runMenu(){
+        System.out.println("Hello !!!!!!");
+        System.out.println("Please give:");
+        System.out.println("Login:");
+        System.out.println("Password:");
+        Scanner scanner = new Scanner(System.in);
+        String logincheck=scanner.nextLine();
+        String passwordcheck=scanner.nextLine();
+        //tutaj metoda (czy jest w bazie taki chłop z takim loginem i hasłem
+        System.out.printf("witaj %s %s na dzienniku",logincheck,passwordcheck);
+    }
 }
