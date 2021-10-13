fdd69a0f458ee0f12817e2e38044c56571df24a5
Franek Krzeszowski <75323319+Fragewskyy@users.noreply.github.com>
2021-10-13 14:50:10 +0200

    role

---
+++ roles.sql
@@ -0,0 +1,10 @@
+INSERT INTO role(role_name)
+VALUES ('Student');
+INSERT INTO role(role_name)
+VALUES ('Guardian');
+INSERT INTO role(role_name)
+VALUES ('Teacher');
+INSERT INTO role(role_name)
+VALUES ('Principal');
+INSERT INTO role(role_name)
+VALUES ('Admin');
