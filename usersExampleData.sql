a7469773edbc1b3585e482e9649fe4b0b6bd65a2
Franciszek Krzeszowski <75323319+Fragewskyy@users.noreply.github.com>
2021-11-16 15:46:57 +0100

    Example data

---
+++ usersExampleData.sql
@@ -0,0 +1,46 @@
+/*
+-- Query: SELECT * FROM dziennik.users
+LIMIT 0, 1000
+
+-- Date: 2021-11-16 15:44
+*/
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (1,'eandrys0@virginia.edu','6T90fyNtNaw','Eachelle','Andrys',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (2,'jsich1@whitehouse.gov','19lOOY','Jennifer','Sich',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (3,'kscholer2@google.com.br','LeLsMYKHSC','Karena','Scholer',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (4,'mscreen3@jimdo.com','MwxbdR7vmBS','Marcus','Screen',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (5,'kdumbrill4@dion.ne.jp','YH4p6f9EZHR','Kendrick','Dumbrill',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (6,'ehounihan5@rakuten.co.jp','D1nT1EhhTn','Eveleen','Hounihan',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (7,'vlaugharne6@google.co.uk','IOWKu8','Valli','Laugharne',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (8,'ekroon7@arstechnica.com','Mc86cTX3wrzE','Erl','Kroon',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (9,'dhowden8@cnn.com','WcMJT90eh','Darda','Howden',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (10,'emarmon9@is.gd','StjXWxdjlfx','Etan','Marmon',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (11,'jscroggesa@wordpress.com','CwbLZXF7nAUn','Josh','Scrogges',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (12,'lpollettb@discovery.com','nlwAxiFlFb','Louisa','Pollett',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (13,'fmackrellc@merriam-webster.com','oabqoB','Fan','Mackrell',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (14,'khickinbottomd@state.gov','Hwt8IIiyLVY','Kiri','Hickinbottom',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (15,'cbritlande@wordpress.com','ZFy7HD3H','Celestia','Britland',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (16,'parkowf@amazon.de','wVPOIDnp','Piggy','Arkow',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (17,'tpammentg@tuttocitta.it','LAYbgRAU','Tabby','Pamment',1);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (18,'hkearleyh@naver.com','QwXJKa6BQ','Harlin','Kearley',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (19,'cmaceveleyi@linkedin.com','4pviRvS0ZSD','Chelsae','MacEveley',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (20,'fgilej@woothemes.com','8A6czHdP','Felice','Gile',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (21,'vbussellk@uiuc.edu','5VD1X8uG7X','Vale','Bussell',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (22,'ebeaulieul@apple.com','VMzwZky','Ermengarde','Beaulieu',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (23,'soquirkm@zdnet.com','5QdmEzRUMX','Sammy','O\' Quirk',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (24,'mmaran@dion.ne.jp','7IiNcsahUe4E','Marrissa','Mara',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (25,'eaitkino@comcast.net','46ueBpTwvd','Elvina','Aitkin',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (26,'abowdreyp@flickr.com','UFlb9ekplj','Amy','Bowdrey',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (27,'ddoringq@gov.uk','ZVSOVZo','Darwin','Doring',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (28,'lhayr@nbcnews.com','bayy8rSDRn','Lyssa','Hay',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (29,'kloachs@spiegel.de','srWxRHwA','Kellen','Loach',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (30,'lbinyont@mac.com','BV7AehdfpE','Loria','Binyon',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (31,'dmanfordu@washingtonpost.com','cAU75VE','Demetris','Manford',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (32,'gmathwenv@samsung.com','UXxpZklDe0c','Gaelan','Mathwen',2);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (33,'ktittw@cbc.ca','BK4UlB9','Korella','Titt',3);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (34,'estockdalex@bigcartel.com','Sb33MsHqj','Eadie','Stockdale',3);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (35,'sbredburyy@edublogs.org','IgHqmK','Sharlene','Bredbury',3);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (36,'afowliez@microsoft.com','omvKgRv','Alli','Fowlie',3);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (37,'rostridge10@nationalgeographic.com','OCo0eEtkC','Robers','Ostridge',3);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (38,'mosherrin11@digg.com','bRYdlogI7','Minda','O\'Sherrin',3);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (39,'lbenettelli12@smugmug.com','6mJUIsofsLZm','Lorettalorna','Benettelli',4);
+INSERT INTO `users` (`user_id`,`login`,`password`,`name`,`surname`,`role_id`) VALUES (40,'cverden13@nbcnews.com','9mA2g7N','Calla','Verden',5);
