ba2b872b470dc0ab52c2dd8cfe20fb1eec973e71
Franciszek Krzeszowski <75323319+Fragewskyy@users.noreply.github.com>
2021-11-16 16:05:22 +0100

    Add files via upload

---
+++ exampleDatasets/classExampleData.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM dziennik.classes
+LIMIT 0, 1000
+
+-- Date: 2021-11-16 16:02
+*/
+INSERT INTO `classes` (`class_id`,`class_name`,`teacher_id`,`students_amount`) VALUES (1,'3a',3,17);
---
+++ exampleDatasets/teacherExampleData.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM dziennik.teacher
+LIMIT 0, 1000
+
+-- Date: 2021-11-16 15:59
+*/
+INSERT INTO `teacher` (`teacher_id`,`user_id`) VALUES (1,33);
+INSERT INTO `teacher` (`teacher_id`,`user_id`) VALUES (2,34);
+INSERT INTO `teacher` (`teacher_id`,`user_id`) VALUES (3,35);
+INSERT INTO `teacher` (`teacher_id`,`user_id`) VALUES (4,36);
+INSERT INTO `teacher` (`teacher_id`,`user_id`) VALUES (5,37);
+INSERT INTO `teacher` (`teacher_id`,`user_id`) VALUES (6,38);
