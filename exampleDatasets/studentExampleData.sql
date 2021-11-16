d2fc4161fee52c3f0eaa51c36a1d4774f7638f4a
Franciszek Krzeszowski <75323319+Fragewskyy@users.noreply.github.com>
2021-11-16 16:28:00 +0100

    Add files via upload

---
+++ exampleDatasets/studentExampleData.sql
@@ -0,0 +1,23 @@
+/*
+-- Query: SELECT * FROM dziennik.student
+LIMIT 0, 1000
+
+-- Date: 2021-11-16 16:25
+*/
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (1,1,1,3,'5512318898');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (2,2,1,3,'4867499676');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (3,3,1,3,'6224127351');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (4,4,1,3,'6474067292');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (5,5,1,3,'4949216679');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (6,6,1,3,'3638016881');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (7,7,1,3,'7677764560');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (8,8,1,3,'1222894805');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (9,9,1,3,'3913052494');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (10,10,1,3,'3612059748');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (11,11,1,3,'8274741637');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (12,12,1,3,'3077295546');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (13,13,1,3,'7857772045');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (14,14,1,3,'3446704707');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (15,15,1,3,'4614054935');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (16,16,1,3,'7438076320');
+INSERT INTO `student` (`student_id`,`user_id`,`class_id`,`teacher_id`,`phone_number`) VALUES (17,17,1,3,'5568341709');
