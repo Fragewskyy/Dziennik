0e04b2658aade2a3f2dea179b02f72dd519b115b
Franciszek Krzeszowski <75323319+Fragewskyy@users.noreply.github.com>
2021-11-16 16:10:44 +0100

    Add files via upload

---
+++ exampleDatasets/adminExampleData.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM dziennik.admin
+LIMIT 0, 1000
+
+-- Date: 2021-11-16 16:06
+*/
+INSERT INTO `admin` (`admin_id`,`user_id`) VALUES (1,40);
---
+++ exampleDatasets/subjectExampleData.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM dziennik.subjects
+LIMIT 0, 1000
+
+-- Date: 2021-11-16 16:09
+*/
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (1,'Polish');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (2,'Biology');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (3,'Chemistry');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (4,'Math');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (5,'Psychics');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (6,'PE');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (7,'IT');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (8,'Religion');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (9,'History');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (10,'Geography');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (11,'Spanish');
+INSERT INTO `subjects` (`subject_id`,`subject_name`) VALUES (12,'English');
