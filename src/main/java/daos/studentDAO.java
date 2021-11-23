011ac388539aae4aa8e927403fa4c484e2c606fb
Franek Krzeszowski <kontakt.fragewskyy@gmail.com>
2021-11-23 19:52:15 +0100

    Guardian i admin interfejsy

--- src/main/java/daos/studentDAO.java
+++ src/main/java/daos/studentDAO.java
@@ -7,6 +7,8 @@ import java.sql.*;
 
 public class studentDAO implements StudentInterface {
     public void displayAllGrades() {
+        String query = "SELECT grade, (SELECT subject_name FROM subjects WHERE grades.subject_id = subjects" +
+                ".subject_id) FROM subjects WHERE student_id = 2";
 
     }
 
