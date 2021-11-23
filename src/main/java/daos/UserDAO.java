393025b104701dbd925687cc9a7e61f24bba19b4
aloix123 <pawel.kupis@o2.pl>
2021-11-23 19:04:12 +0100

    Guardian i admin interfejsy

--- src/main/java/daos/UserDAO.java
+++ src/main/java/daos/UserDAO.java
@@ -8,13 +8,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class UserDAO implements UserInterface {
-    public void sendTextMessage() {
+    public void sendTextMessage() throws SQLException {
 
+        String query="INSERT INTO dziennik.messeges VALUSE("
+        Connection connection= DriverManager.getConnection(SQLconector.getUrl(), SQLconector.getName(), SQLconector.getPassword());
+        Statement statement=connection.createStatement();
     }
 
     public void changePassword(String newpassword,String login,String password) throws SQLException {
         String query="update dziennik.users  set password='"+newpassword+"' where login='"+login+"' AND password='"+password+"';";
-        Connection connection= DriverManager.getConnection(SQLmanager.getUrl(), SQLmanager.getName(), SQLmanager.getPassword());
+        Connection connection= DriverManager.getConnection(SQLconector.getUrl(), SQLconector.getName(), SQLconector.getPassword());
         Statement statement=connection.createStatement();
         statement.executeUpdate(query);
         System.out.println("ustawiłeś  nowe hasło:"+newpassword);
