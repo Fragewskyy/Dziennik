297e8b0803700c4a7bbf1aec553c90ec3e94e08a
Franek Krzeszowski <kontakt.fragewskyy@gmail.com>
2021-11-27 17:06:01 +0100

    Naprawiłem błąd przy ładowaniu view i wszystko śmiga

--- src/main/java/daos/studentDAO.java
+++ src/main/java/daos/studentDAO.java
@@ -7,8 +7,7 @@ import java.sql.*;
 
 public class studentDAO implements StudentInterface {
     public void displayAllGrades() {
-        String query = "SELECT grade, (SELECT subject_name FROM subjects WHERE grades.subject_id = subjects" +
-                ".subject_id) FROM subjects WHERE student_id = 2";
+        String query = "SELECT (SELECT subject_name FROM subjects WHERE grades.subject_id = subjects.subject_id) AS Subject_name, grade FROM grades WHERE student_id = 2";
 
     }
 
