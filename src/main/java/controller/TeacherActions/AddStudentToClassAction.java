d91fa6d0b2c5538065867277a5da4cd5cda55e5f
aloix123 <pawel.kupis@o2.pl>
2022-01-01 22:15:00 +0100

    modify grade action in progres

--- src/main/java/controller/TeacherActions/AddStudentToClassAction.java
+++ src/main/java/controller/TeacherActions/AddStudentToClassAction.java
@@ -1,18 +1,35 @@
 package controller.TeacherActions;
 
 import controller.Action;
+import controller.SQLController;
 import model.User;
 import repository.ClassDAO;
+import repository.StudentDAO;
 import repository.TeacherDAO;
 import repository.UserDAO;
+import view.MainView;
 
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Scanner;
 
 public class AddStudentToClassAction implements Action {
     @Override
     public void executeQuery() {
-
+        ClassDAO classDAO=new ClassDAO();
+        StudentDAO studentdaoo=new StudentDAO();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("what class ???");
+        String studentid=studentdaoo.getstudentid(MainView.getLogin());
+        String classid=classDAO.getclassbyname(scanner.next());
+        String query="Update dziennik.students SET  class_id="+classid+" where student_id="+studentid+";";
+        try {
+            Connection connection = DriverManager.getConnection(SQLController.URL, SQLController.USERNAME,SQLController.PASSWORD);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println(" new class has been assigned");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
