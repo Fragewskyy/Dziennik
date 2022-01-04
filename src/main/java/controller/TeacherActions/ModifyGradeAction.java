93ed18e890d648365889caff702070bb7774b067
aloix123 <pawel.kupis@o2.pl>
2022-01-04 16:40:41 +0100

    modify grade actiondone :)

--- src/main/java/controller/TeacherActions/ModifyGradeAction.java
+++ src/main/java/controller/TeacherActions/ModifyGradeAction.java
@@ -5,34 +5,34 @@ import controller.SQLController;
 import model.peoplesRoles.Teacher;
 import repository.StudentDAO;
 import repository.TeacherDAO;
+import view.MainView;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class ModifyGradeAction implements Action {
-    public static Map<Integer,String> creategrademap(){
+    public static Map<Integer,List<String>> creategrademap(){
         TeacherDAO teacherDAO=new TeacherDAO();
         StudentDAO studentDAO=new StudentDAO();
         Scanner scanner = new Scanner(System.in);
         System.out.println("student login pls");
         String studentid=studentDAO.getstudentid(scanner.nextLine());
-        String teachersubject=String.valueOf(teacherDAO.getsubject());
-        String show="SELECT grade ,date FROM dziennik.grades inner join dziennik.lessons on dziennik.grades.lesson=dziennik.lessons.lesson_id where student_id="+studentid+" and (SELECT subject_name FROM dziennik.subjects where dziennik.lessons.subject_id=dziennik.subjects.subject_id)  ='"+teachersubject+"'  ;";
+        String teacherid=teacherDAO.getteacherid(MainView.getLogin());
+        String show="SELECT grade_id,(SELECT subject_name FROM dziennik.subjects where dziennik.lessons.subject_id=dziennik.subjects.subject_id) ,grade ,date FROM dziennik.grades inner join dziennik.lessons on dziennik.grades.lesson=dziennik.lessons.lesson_id where \n" +
+                "                student_id="+studentid+" and grades.lesson in (SELECT lesson_id FROM dziennik.lessons where teacher_id="+teacherid+")  ;";
         Connection connection = null;
         try {
             connection = SQLController.Connect();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(show);
             int index = 1;
-            Map<Integer, String> grademaplist = new HashMap<Integer, String>();
+            Map<Integer, List<String>> grademaplist = new HashMap<Integer, List<String>>();
             while (resultSet.next()) {
-                grademaplist.put(index, resultSet.getString("grade") + ":" + resultSet.getString("date"));
+                List<String> helper=Arrays.asList(new String[]{resultSet.getString("grade"), resultSet.getString("date"),String.valueOf(resultSet.getInt("grade_id"))});
+                grademaplist.put(index, helper);
                 index++;
             }
             return grademaplist;
@@ -45,14 +45,29 @@ public class ModifyGradeAction implements Action {
     @Override
     public void executeQuery() {
         Scanner scanner = new Scanner(System.in);
-        Map<Integer, String> finalgradesMapList=ModifyGradeAction.creategrademap();
+        Map<Integer, List<String>> finalgradesMapList=ModifyGradeAction.creategrademap();
         System.out.println("select your grade");
 
-        for(int listindex=0; listindex<finalgradesMapList.size(); listindex++) {
-            finalgradesMapList.get(listindex);
+        for(int listindex=1; listindex<finalgradesMapList.size(); listindex++) {
+            System.out.println(listindex+"="+finalgradesMapList.get(listindex).get(0)+":"+finalgradesMapList.get(listindex).get(1));
         }
-        //todo
-        //ten no zrób metode zmodufikuj ocene tutaj tylko, i wogule dokończ to co teraz widzisz
+        int inputnumber=scanner.nextInt();
+        System.out.println("what new grade");
+        String newgrade=scanner.next();
+
+        List<String> finalgrade=Arrays.asList(new String[]{finalgradesMapList.get(inputnumber).get(0), newgrade});
+        finalgradesMapList.replace(inputnumber,finalgrade);
+        String query="update dziennik.grades set grade=3 where grade_id="+finalgradesMapList.get(inputnumber).get(2)+";";
+        Connection connection = null;
+        try {
+            connection = SQLController.Connect();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("you updated a grade ,nice bro");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 
@@ -61,3 +76,5 @@ public class ModifyGradeAction implements Action {
         return "modify grade ";
     }
 }
+
+//ten no zrób metode zmodufikuj ocene tutaj tylko, i wogule dokończ to co teraz widzisz
