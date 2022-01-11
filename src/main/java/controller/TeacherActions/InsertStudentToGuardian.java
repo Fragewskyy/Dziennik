1653add422aa5fd739e56c355f519a7405552650
student <asdasdasdfa>
2022-01-11 16:22:39 +0100

    AddStudentToClassAction.java - done :)

--- src/main/java/controller/TeacherActions/InsertStudentToGuardian.java
+++ src/main/java/controller/TeacherActions/InsertStudentToGuardian.java
@@ -1,11 +1,35 @@
 package controller.TeacherActions;
 
 import controller.Action;
+import controller.SQLController;
+import repository.GuardianDAO;
+import repository.StudentDAO;
+import view.MainView;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class InsertStudentToGuardian implements Action {
     @Override
     public void executeQuery() {
+        StudentDAO student=new StudentDAO();
+        GuardianDAO guardianDAO=new GuardianDAO();
+        System.out.println("what student (write login)??");
+        Scanner scanner = new Scanner(System.in);
 
+        String studentid=student.getstudentid(scanner.next());
+        String guardianid=guardianDAO.getguardianid(MainView.getLogin());
+        String query="Update dziennik.student SET guardian_id="+guardianid+" where student_id="+studentid+";";
+        try {
+            Connection connection= SQLController.Connect();
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("new guardian has been inserted :)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
