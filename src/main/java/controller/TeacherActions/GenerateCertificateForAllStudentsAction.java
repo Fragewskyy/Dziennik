ee1932979f05892800079d339f2d8bb2a1ad8c61
aloix123 <pawel.kupis@o2.pl>
2022-01-23 12:30:21 +0100

    zrobiłem po części GenerateCertificateForAllStudentsAction , ale po części , musze jeszcze to wsztstko zapisywac do jakiegoś specyficznego folderu, i ten pdf jest brzydki , ale jest

--- src/main/java/controller/TeacherActions/GenerateCertificateForAllStudentsAction.java
+++ src/main/java/controller/TeacherActions/GenerateCertificateForAllStudentsAction.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
 public class GenerateCertificateForAllStudentsAction implements Action {
     public static ArrayList<Integer> showstudentsfromclass(String classid){
         java.util.List<Integer> result = new ArrayList<Integer>();
-        String query = "SELECT student_id FROM dziennik.students where class="+classid+";";
+        String query = "SELECT student_id FROM dziennik.students where class_id="+classid+";";
         try {
             Connection connection= SQLController.Connect();
             Statement statement = connection.createStatement();
@@ -36,31 +36,43 @@ public class GenerateCertificateForAllStudentsAction implements Action {
     public void executeQuery() {
         ClassDAO classDAO=new ClassDAO();
         Scanner scanner = new Scanner(System.in);
-        String classname=scanner.next();
+        System.out.println(" write your class name");
+        String classname=scanner.nextLine();
         String classid=classDAO.getclassbyname(classname);
         ArrayList<Integer> classStudents=showstudentsfromclass(classid);
-        Document document = new Document();
-        for(int studentid:classStudents) {
 
-            String query="SELECT (SELECT name FROM dziennik.users where dziennik.users.user_id=dziennik.students.user_id) as name,(SELECT surname FROM dziennik.users where dziennik.users.user_id=dziennik.students.user_id) as surname from dziennik.students where student_id="+studentid+";";
+        for(int studentid:classStudents) {
+            Document document = new Document();
+
+            String getNameAndSurnaemQuery="SELECT (SELECT name FROM dziennik.users where dziennik.users.user_id=dziennik.students.user_id) as name,(SELECT surname FROM dziennik.users where dziennik.users.user_id=dziennik.students.user_id) as surname from dziennik.students where student_id="+String.valueOf(studentid)+";";
             try {
                 Connection connection=SQLController.Connect();
                 Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(query);
-                PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+                ResultSet resultSet = statement.executeQuery(getNameAndSurnaemQuery);
+                resultSet.next();
+                String namsurname=resultSet.getString("name")+resultSet.getString("surname");
+                System.out.println(namsurname);
+                PdfWriter.getInstance(document, new FileOutputStream(namsurname));
             } catch (DocumentException e) {
                 e.printStackTrace();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            document.open();
             Font font = FontFactory.getFont(FontFactory.COURIER, 32, BaseColor.BLACK);
-            Chunk chunk = new Chunk("Hello World", font);
-
+            Connection connection= null;
+            document.open();
+            String showgradesQuery="SELECT (SELECT (SELECT subject_name FROM dziennik.subjects where dziennik.lessons.subject_id=dziennik.subjects.subject_id) FROM dziennik.lessons where dziennik.grades.lesson=dziennik.lessons.lesson_id) as subject, round(avg(grade)) as grade  FROM dziennik.grades where student_id="+studentid+" group by subject;";
             try {
-                document.add(chunk);
-            } catch (DocumentException e) {
-                e.printStackTrace();
+                connection = SQLController.Connect();
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(showgradesQuery);
+                while(resultSet.next()) {
+                    String content=resultSet.getString("subject")+ resultSet.getInt("grade");
+                    Chunk chunk = new Chunk(content, font);
+                    document.add(chunk);
+                }
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
             }
             document.close();
 
