f5d8d5221493a9fc4f152fe8dc3acfe1c6ada937
Franek Krzeszowski <kontakt.fragewskyy@gmail.com>
2022-02-01 01:20:48 +0100

    na wszelki wypadek

--- src/main/java/controller/AdminActions/TurnOffOnpasswordAction.java
+++ src/main/java/controller/AdminActions/TurnOffOnpasswordAction.java
@@ -4,6 +4,7 @@ import controller.Action;
 import controller.SQLController;
 
 import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,22 +14,45 @@ public class TurnOffOnpasswordAction implements Action {
 
     @Override
     public void executeQuery() throws SQLException {
+
+        SQLController.Connect();
+        Statement statement = SQLController.Connect().createStatement();
+        Scanner scanner = new Scanner(System.in);
+        int decision;
         getPasswordCheckingValue();
         if(passChecking) {
             System.out.println("Password checking is now enabled.");
         } else {
             System.out.println("Password checking is now disabled.");
         }
-        System.out.print("Press <1> to enable password checking, otherwise click <0>: ");
-        Scanner scanner = new Scanner(System.in);
-        int decision = scanner.nextInt();
+        Task:
+        while (true) {
+
+            System.out.print("Press <1> to enable password checking, otherwise click <0>: ");
+            try {
+                decision = scanner.nextInt();
+                if (decision == 1 || decision == 0) {
+                    break;
+                }
+                System.out.println("Incorrect choice. Try again!");
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect choice. Try again!");
+                scanner.next();
+            }
+
+        }
         if (decision == 1) {
+            String q = "Update admin SET password_check = 1;";
+            statement.executeUpdate(q);
             System.out.println("Password checking is now enabled.");
-            passChecking = false;
+
+            passChecking = true;
         }
         else if (decision == 0) {
+            String q1 = "Update admin SET password_check = 0;";
+            statement.executeUpdate(q1);
             System.out.println("Password checking is now disabled.");
-            passChecking = true;
+            passChecking = false;
         }
         else {
             System.out.println("Wrong number");
