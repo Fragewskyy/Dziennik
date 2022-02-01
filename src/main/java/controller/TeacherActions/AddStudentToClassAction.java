d795653cffe052c099d242edb53b91c959e8adfe
Franek Krzeszowski <kontakt.fragewskyy@gmail.com>
2022-02-01 02:50:52 +0100

    2:50 kurna zaraz bedzie jasno a tu tyle bledow

--- src/main/java/controller/TeacherActions/AddStudentToClassAction.java
+++ src/main/java/controller/TeacherActions/AddStudentToClassAction.java
@@ -10,39 +10,61 @@ import repository.TeacherDAO;
 import repository.UserDAO;
 
 import java.sql.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AddStudentToClassAction implements Action {
     @Override
     public void executeQuery() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+
+
         UserDAO userDAO = new UserDAO();
         StudentDAO studentDAO = new StudentDAO();
         Connection connection = DriverManager.getConnection(SQLController.URL, SQLController.USERNAME,SQLController.PASSWORD);
         Statement statement = connection.createStatement();
-        int iter = 0;
+
         for (Student student : studentDAO.getAll()) {
-            iter += 1;
+
             String query = "SELECT class_name from classes WHERE class_id = (select class_id from student where " +
                     "student_id = " + student.studentId + ");";
             ResultSet resultSet = statement.executeQuery(query);
             resultSet.next();
             try {
-                System.out.println(iter + ". " + userDAO.get(student.userId).name + " " + userDAO.get(student.userId).surname + " | Current class: " + resultSet.getString("class_name"));
+                System.out.println(student.studentId + ". " + userDAO.get(student.userId).name + " " + userDAO.get(student.userId).surname + " | Current class: " + resultSet.getString("class_name"));
             } catch (SQLException e) {
-                System.out.println(iter + ". " + userDAO.get(student.userId).name + " " + userDAO.get(student.userId).surname + " | Not assigned to any class");
+                System.out.println(student.studentId + ". " + userDAO.get(student.userId).name + " " + userDAO.get(student.userId).surname + " | Not assigned to any class");
             }
         }
+        int choice;
 
-        System.out.print("Select the student you want to assign (Type ID): ");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
+        while (true) {
+            System.out.print("Select the student you want to assign (Type ID): ");
+            try {
+                choice = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Try again!");
+                scanner.next();
+            }
+        }
         String query1 = "SELECT * FROM classes;";
         ResultSet resultSet1 = statement.executeQuery(query1);
         while (resultSet1.next()){
             System.out.println(resultSet1.getInt("class_id") + ". " + resultSet1.getString("class_name"));
         }
-        System.out.print("Select the class to which you want to assign the student: ");
-        int choice1 = scanner.nextInt();
+
+        int choice1;
+        while (true) {
+            System.out.print("Select the class to which you want to assign the student: ");
+            try {
+                choice1 = scanner.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Try again!");
+                scanner.next();
+            }
+        }
         String query2 = "UPDATE student SET class_id = " + choice1 + " WHERE student_id = " + choice + ";";
         statement.executeUpdate(query2);
         System.out.println("Student successfully assigned.");
--- src/main/java/controller/TeacherActions/GenerateStudentCertificateAction.java
+++ src/main/java/controller/TeacherActions/GenerateStudentCertificateAction.java
@@ -5,33 +5,62 @@ import controller.GenerateCertificate;
 import controller.SQLController;
 import model.peoplesRoles.Student;
 import repository.StudentDAO;
+import repository.UserDAO;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GenerateStudentCertificateAction implements Action {
     @Override
-    public void executeQuery() {
+    public void executeQuery() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        UserDAO userDAO = new UserDAO();
         StudentDAO studentDAO = new StudentDAO();
-        System.out.println("SELECT student ");
-        ArrayList<Student> studentschoicelist = null;
-        try {
-            studentschoicelist = studentDAO.getAll();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+
+        Connection connection= SQLController.Connect();
+        Statement statement= connection.createStatement();
+
+        int iter = 0;
+
+        for (Student student : studentDAO.getAll()) {
+            iter+=1;
+            String query = "SELECT class_name from classes WHERE class_id = (select class_id from student where " +
+                    "student_id = " + student.studentId + ");";
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.next();
+            try {
+                System.out.println(student.studentId + ". " + userDAO.get(student.userId).name + " " + userDAO.get(student.userId).surname + " | Current class: " + resultSet.getString("class_name"));
+            } catch (SQLException e) {
+                System.out.println(student.studentId + ". " + userDAO.get(student.userId).name + " " + userDAO.get(student.userId).surname + " | Not assigned to any class");
+            }
         }
+        int choice;
+
+        while (true) {
+            System.out.print("Select the student you want to assign (Type ID): ");
+            try {
+                choice = scanner.nextInt();
+                if(choice>=1 && choice<=iter) {
+                    break;
+                }
+                System.out.println("Try again!");
+            } catch (InputMismatchException e) {
+                System.out.println("Try again!");
+                scanner.next();
+            }
+        }
+        Student student = studentDAO.get(choice);
         int index=1;
 
-        for (Student student : studentschoicelist) {
+
             GenerateCertificate generateCertificate = new GenerateCertificate();
             String getuserNameAndSurnameQuery="SELECT name,surname FROM dziennik.users where user_id="+student.userId+";";
             try {
-                Connection connection= SQLController.Connect();
-                Statement statement= connection.createStatement();
                 ResultSet resultSet = statement.executeQuery(getuserNameAndSurnameQuery);
                 while (resultSet.next()) {
                     System.out.println(index+":"+resultSet.getString("name")+" "+resultSet.getString("surname"));
@@ -42,15 +71,12 @@ public class GenerateStudentCertificateAction implements Action {
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
-            Scanner scanner = new Scanner(System.in);
-            Student selectedStudent=studentschoicelist.get(scanner.nextInt()-1);
+
             try {
-                generateCertificate.executeQuery(selectedStudent.getStudentId());
+                generateCertificate.executeQuery(choice);
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
-
-        }
     }
 
     @Override
